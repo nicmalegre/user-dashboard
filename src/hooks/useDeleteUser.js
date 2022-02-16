a793ea67c6dca4a2016b9cbc835a09599236dfd3
nicmalegre <40902023+nicmalegre@users.noreply.github.com>
2022-02-15 22:37:07 -0300

    Fix endpoints/ids used in api calls

--- src/hooks/useDeleteUser.js
+++ src/hooks/useDeleteUser.js
@@ -10,7 +10,7 @@ export const useDeleteUser = () => {
     if (!userId) return;
     setIsLoading(true);
     try {
-      await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`, {
+      await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
         method: "DELETE",
       });
 
